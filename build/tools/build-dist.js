5421c78adc26b6667d9e83aa9756b30aff65ffcc
Yuri Guller <gullerya@gmail.com>
2018-10-20 22:36:58 +0300

    tech: adding travis CI

--- build/tools/build-dist.js
+++ build/tools/build-dist.js
@@ -1,18 +1,8 @@
-'use strict';
-
 const
 	os = require('os'),
-	fsExtra = require('fs-extra'),
-	concater = require('./build-concat.js'),
-	minifier = require('./build-minify.js');
+	fsExtra = require('fs-extra');
 
 process.stdout.write('*** CLEANUP ***' + os.EOL + os.EOL);
 fsExtra.emptyDirSync('./dist');
-
-process.stdout.write('*** CONCATENATION ***' + os.EOL + os.EOL);
-concater.execute();
-
-process.stdout.write('*** MINIFICATION ***' + os.EOL + os.EOL);
-minifier.execute();
 
 process.stdout.write('*** DONE ***' + os.EOL + os.EOL);
