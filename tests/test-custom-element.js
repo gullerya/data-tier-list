47b9913f5c3a6cbdbde9a15c075a75c1724e4162
gullerya <gullerya@gmail.com>
2020-08-07 01:12:53 +0300

    adding specialized test

--- tests/test-custom-element.js
+++ tests/test-custom-element.js
@@ -1,8 +1,8 @@
 import { getSuite } from '../node_modules/just-test/dist/just-test.js';
-import * as DataTier from '../dist/data-tier/data-tier.js';
+import * as DataTier from '../dist/data-tier/data-tier.min.js';
 import '../dist/data-tier-list.js';
 
-const suite = getSuite({ name: 'Complex cases - grid' });
+const suite = getSuite({ name: 'Complex cases - custom elements nested structure' });
 
 customElements.define('list-in-ce-a', class extends HTMLElement {
 	constructor() {
