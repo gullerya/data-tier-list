5b1012e52c66a5a41cdd1a1faa86dbccdf13888d
Yuri Guller <gullerya@gmail.com>
2021-04-25 10:09:59 +0300

    wip

--- ci/tools/build-dist.js
+++ ci/tools/build-dist.js
@@ -1,26 +1,25 @@
-import os from 'os';
 import fs from 'fs';
-import process from 'process';
 import fsExtra from 'fs-extra';
 import uglifyES from 'uglify-es';
 
-process.stdout.write('*** CLEANUP ***' + os.EOL);
+console.info('*** CLEANUP ***');
 fsExtra.emptyDirSync('./dist');
 
-process.stdout.write('*** BUILD ***' + os.EOL);
-// fsExtra.copySync('./node_modules/data-tier/dist', './src/data-tier');
-// fsExtra.removeSync('./src/data-tier/data-tier.js');
-// fsExtra.removeSync('./src/data-tier/dom-processor.js');
-// fsExtra.removeSync('./src/data-tier/ties.js');
-// fsExtra.removeSync('./src/data-tier/utils.js');
-// fsExtra.removeSync('./src/data-tier/views.js');
+console.info('*** COPY ***');
 fsExtra.copySync('./src', './dist');
 
-process.stdout.write('*** MINIFY ***' + os.EOL);
+console.info('*** PROCESS IMPORT MAPS ***');
+processImportMaps();
+
+console.info('*** MINIFY ***');
 fs.writeFileSync(
 	'dist/data-tier-list.min.js',
 	uglifyES.minify({ 'dist/data-tier-list.min.js': fs.readFileSync('dist/data-tier-list.js', { encoding: 'utf8' }) }).code
 );
 
-process.stdout.write('*** DONE ***' + os.EOL);
+console.info('*** DONE ***');
 fsExtra.copySync('./src/data-tier-list.js', './dist/data-tier-list.js');
+
+function processImportMaps() {
+	//	TODO
+}
